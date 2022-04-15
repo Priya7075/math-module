bee231d9be47191db7826c758d5bfc51134f77e1
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-15 12:42:54 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,79 @@
+import math
+
+# print(math.ceil(0.1))#1
+# print(math.ceil(2.4))#3
+# print(math.ceil(5.3))#6
+# print(math.ceil(-5.3))#-5
+# print(math.ceil(22.6))#23
+# print(math.ceil(10.0))#10
+# print(math.exp(1))#2.718281828459045
+# print(math.exp(3))#20.085536923187668
+# print(math.ceil(0.1))#1
+# print(math.factorial(0))#1
+# print(math.factorial(1))#1
+# print(math.factorial(2))#2
+# print(math.factorial(3))#6
+# print(math.factorial(4))#24
+# print(math.factorial(5))#120
+# print(math.floor(23.4))#23
+# print(math.floor(23.5))#23
+# print(math.floor(23.6))#23
+# print(math.floor(-23.6))#-24
+# print(math.floor(-19.0))#-19
+# print(math.floor(-19.1))#-20
+# print(math.floor(18.27))#18
+# print(math.ceil(75.0))#75
+# print(math.ceil(75.3))#76
+# print(math.floor(75.3))#75
+# print(math.ceil(75.5))#76
+# a=math.pow(3,3)
+# b=3**3
+# print(a,type(a))#27.0
+# print(b,type(b))#27
+# print(math.pi)#3.141592653589793
+# print(math.pi*2)#6.283185307179586
+# print(math.sqrt(4))#2.0
+# print(math.sqrt(6.7))#2.588435821108957
+# print(math.sqrt(24))#4.898979485566356
+# print(math.sqrt(25))#5.0
+# c=2
+# d=6
+# e=14
+# f=0
+# print(math.isqrt(c))#1
+# print(math.isqrt(d))#2
+# print(math.isqrt(e))#3
+# print(math.isqrt(f))#0
+# n=[1,2,3,4,5]
+# print(sum(n))#15
+# print(math.fsum(n))#15.0
+# print(math.prod(n))#120
+# print(int(math.fsum(n)))#15
+# g=17%3
+# print(g)#2
+# print(math.fmod(17,3))#2.0
+# v=-4
+# print(abs(v))#4
+# print(math.fabs(v))#4.0
+# print(math.cos(45))#0.525321988
+# print(math.sin(45))#0.8509035
+# print(math.an(45))#1.619775190
+# print(math.trunc(4.55))#4
+# print(math.trunc(-6.55))#-6
+# print(math.gcd(18,11))#1
+# print(math.gcd(18,13))#1
+# print(math.gcd(18,17))#1
+# print(math.gcd(18,19))#1
+# print(math.gcd(18,23))#1
+# print(math.gcd(18,29))#1
+# print(math.gcd(18,31))#1
+# print(math.gcd(18,37))#1
+# print(math.gcd(18,41))#1
+# print(math.gcd(18,43))#1
+# print(math.gcd(18,47))#1
+print(math.gcd(24,32))#8
+print(math.gcd(28,64))#
+print(math.gcd(52,86))
+print(math.gcd(6,16))
+print(math.lcm(6,16))
+print(math.lcm(8,14))
